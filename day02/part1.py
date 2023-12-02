fdbf7d2b859746f5b564c33da28286dbe00fa3dd
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-02 05:18:35 +0000

    day02 2023

--- day02/part1.py
+++ day02/part1.py
@@ -1,3 +1,24 @@
-with open("day02/test.in", "r") as f:
+with open("day02/input.in", "r") as f:
     data = f.readlines()
-                
+
+import re
+
+possible = 0
+
+for index, line in enumerate(data):
+    red_total = map(int, re.findall(r"\d+(?= red)", line))
+    green_total = map(int, re.findall(r"\d+(?= green)", line))
+    blue_total = map(int, re.findall(r"\d+(?= blue)", line))
+    
+    if any(x > 12 for x in red_total):
+        continue
+
+    if any(x > 13 for x in green_total):
+        continue
+
+    if any(x > 14 for x in blue_total):
+        continue
+
+    possible += index + 1
+
+print(possible)
--- day02/part2.py
+++ day02/part2.py
@@ -1,3 +1,17 @@
-with open("day02/test2.in", "r") as f:
+with open("day02/input.in", "r") as f:
     data = f.readlines()
-                
+
+import re
+
+powersum = 0
+
+for index, line in enumerate(data):
+    red_total = max(map(int, re.findall(r"\d+(?= red)", line)))
+    green_total = max(map(int, re.findall(r"\d+(?= green)", line)))
+    blue_total = max(map(int, re.findall(r"\d+(?= blue)", line)))
+
+    power = red_total * blue_total * green_total
+
+    powersum += power
+
+print(powersum)
