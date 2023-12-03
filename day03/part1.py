1331985a51dca796a5152c17e2e9093608ca9ac5
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-03 05:54:00 +0000

    day03 2023

--- day03/part1.py
+++ day03/part1.py
@@ -1,3 +1,19 @@
-with open("day03/test.in", "r") as f:
+with open("day03/input.in", "r") as f:
     data = f.readlines()
-                
+import re
+numbers = []
+characters = []
+
+for i, line in enumerate(data):
+	line = line.strip()
+	for element in re.finditer(r'[^\d\.]', line):
+		characters.append([element.start(0), i, element.group(0)])
+	for num in re.finditer(r'\d+', line):
+		numbers.append([num.start(0), num.end(0), i, int(num.group(0))])
+
+total = 0
+for num in numbers:
+	for char in characters:
+		if char[0] <= num[1] and abs(num[2] - char[1]) <= 1 and char[0] >= num[0]-1:
+			total += num[3]
+print(total)
--- day03/part2.py
+++ day03/part2.py
@@ -1,3 +1,23 @@
-with open("day03/test2.in", "r") as f:
+with open("day03/input.in", "r") as f:
     data = f.readlines()
-                
+import re
+numbers = []
+characters = []
+
+for i, line in enumerate(data):
+	line = line.strip()
+	for element in re.finditer(r'[^\d\.]', line):
+		characters.append([element.start(0), i, element.group(0)])
+	for num in re.finditer(r'\d+', line):
+		numbers.append([num.start(0), num.end(0), i, int(num.group(0))])
+
+total = 0
+
+gears = [x for x in characters if x[2] == '*']
+
+for gear in gears:
+	adjacent = [num for num in numbers if (gear[0] <= num[1] and abs(num[2] - gear[1]) <= 1 and gear[0] >= num[0]-1)]
+	if len(adjacent) == 2:
+		total += adjacent[0][3] * adjacent[1][3]
+
+print(total)
