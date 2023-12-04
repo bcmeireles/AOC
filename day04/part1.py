cac49fe3d832f251b3c4018df194d175f03a3fd9
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-04 05:32:15 +0000

    day04 2023

--- day04/part1.py
+++ day04/part1.py
@@ -1,3 +1,26 @@
-with open("day04/test.in", "r") as f:
+with open("day04/input.in", "r") as f:
     data = f.readlines()
-                
+total = 0
+
+for i, line in enumerate(data):
+    line = line.strip()
+    winning, nums = line.split('|')
+    winning = winning.split(':')[1].split(' ')
+    nums = nums.split(' ')
+
+    winners = 0
+
+    for x in winning:
+        if x == '':
+            continue
+        if x in nums:
+            winners += 1
+        if winners == 0:
+            continue
+    
+    if winners == 1:
+        total += 1
+    elif winners > 1:
+        total += 2 ** (winners - 1)
+
+print(total)
--- day04/part2.py
+++ day04/part2.py
@@ -1,3 +1,31 @@
-with open("day04/test2.in", "r") as f:
+with open("day04/input.in", "r") as f:
     data = f.readlines()
-                
+
+totals = {1: 1}
+
+for i, line in enumerate(data):
+    if i + 1 not in totals.keys():
+        totals[i + 1] = 1
+
+    line = line.strip()
+    winning, nums = line.split('|')
+    winning = winning.split(':')[1].split(' ')
+    nums = nums.split(' ')
+
+    winners = 0
+
+    for x in winning:
+        if x == '':
+            continue
+        if x in nums:
+            winners += 1
+    if winners == 0:
+        continue
+
+    for j in range(i + 2, i + winners + 2):
+        if j in totals.keys():
+            totals[j] += totals[i + 1]
+        else:
+            totals[j] = 1 + totals[i + 1]
+
+print(sum([totals[x] for x in totals.keys()]))
