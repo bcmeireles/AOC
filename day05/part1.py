75e49e9157caf4043d6ba81a1b2ea6cf1f9b783b
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-05 18:57:11 +0000

    day05 2023
    took longer to upload since I wanted to clean it up (the 5:40 am version was terrible)

--- day05/part1.py
+++ day05/part1.py
@@ -1,3 +1,30 @@
-with open("day05/test.in", "r") as f:
+with open("day05/input.in", "r") as f:
     data = f.readlines()
-                
+    
+seeds = list(map(int, data[0].strip("seeds: ").split()))
+
+maps = []
+new_map = []
+
+for line in data[3:]:
+    if "map:\n" in line:
+        maps.append(new_map)
+        new_map = []
+        continue
+    if line != "\n":
+        new_map.append([int(x) for x in line.strip().split()])
+
+maps.append(new_map)
+
+for map in maps:
+    mapped = []
+    for seed in seeds:
+        for destination, source, range in map:
+            if source <= seed <= source + range:
+                mapped.append(destination + seed - source)
+                break
+        else:
+            mapped.append(seed)
+    seeds = mapped
+
+print(min(seeds))
--- day05/part2.py
+++ day05/part2.py
@@ -1,3 +1,47 @@
-with open("day05/test2.in", "r") as f:
+with open("day05/input.in", "r") as f:
     data = f.readlines()
-                
+    
+seeds = list(map(int, data[0].strip("seeds: ").split()))
+
+maps = []
+new_map = []
+
+for line in data[3:]:
+    if "map:\n" in line:
+        maps.append(new_map)
+        new_map = []
+        continue
+    if line != "\n":
+        new_map.append([int(x) for x in line.strip().split()])
+
+maps.append(new_map)
+
+
+merged = []
+for i in range(0, len(seeds), 2):
+    merged.append((seeds[i], seeds[i] + seeds[i + 1] - 1))
+
+all = []
+for map in maps:
+    new = [(s, s + r - 1, d, d + r - 1) for d, s, r in map]
+    all.append(new)
+
+for map in all:
+    new = []
+    control = merged
+    while control:
+        first, second = control.pop()
+        for source, treshold, destination, last in map:
+            if second < source or first > treshold:
+                continue
+            if first < source:
+                control.append((first, source - 1))
+            if second > treshold:
+                control.append((treshold + 1, second))
+            new.append((max(destination, destination + first - source), min(destination + second - source, last)))
+            break
+        else:
+            new.append((first, second))
+    merged = new
+
+print(min(merged)[0])
