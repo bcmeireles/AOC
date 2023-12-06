73baec5d38c552fd27ae807e7eae97f51d2f5ba8
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-06 05:30:30 +0000

    day06 2023

--- day06/part1.py
+++ day06/part1.py
@@ -1,3 +1,27 @@
-with open("day06/test.in", "r") as f:
+with open("day06/input.in", "r") as f:
     data = f.readlines()
                 
+res = 1
+
+lasts = list(map(int, data[0].split()[1:]))
+records = list(map(int, data[1].split()[1:]))
+
+for i, x in enumerate(lasts):
+    racing = records[i] - x
+    raced = 0
+    sum = 0
+
+    for held in range(1, records[i]):
+        raced = x - held
+        distance = raced * held
+
+
+        if raced == distance == 0:
+            break
+
+        if distance > records[i]:
+            sum += 1
+    
+    res *= sum
+
+print(res)
--- day06/part2.py
+++ day06/part2.py
@@ -1,3 +1,20 @@
-with open("day06/test2.in", "r") as f:
+with open("day06/input.in", "r") as f:
     data = f.readlines()
                 
+sum = 0
+
+lasts = int("".join(data[0].split()[1:]))
+record = int("".join(data[1].split()[1:]))
+
+racing = record - lasts
+raced = 0
+
+for held in range(1, record):
+    raced = lasts - held
+    distance = raced * held
+    if raced == distance == 0:
+        break
+    if distance > record:
+        sum += 1
+
+print(sum)
