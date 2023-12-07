d9de252f270528493414c7ec421044bee0e3eb52
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-07 15:35:48 +0000

    day07 2023

--- day07/part1.py
+++ day07/part1.py
@@ -1,3 +1,32 @@
-with open("day07/test.in", "r") as f:
+with open("day07/input.in", "r") as f:
     data = f.readlines()
                 
+order = "23456789TJQKA"
+patterns = (1, 1, 1, 1, 1), (1, 1, 1, 2), (1, 2, 2), (1, 1, 3), (2, 3), (1, 4), (5,)
+
+parsed = []
+total = 0
+
+for line in data:
+    count = {}
+    hand, score = line.strip().split()
+
+    for x in hand:
+        if x not in count.keys():
+            count[x] = 1
+        else:
+            count[x] += 1
+    
+    vals = []
+    for x in hand:
+        vals.append(order.index(x))
+
+    pattern = tuple(sorted(count.values()))
+    pattern_val = patterns.index(pattern)
+
+    parsed.append((pattern_val, vals, score))
+
+for i, (_, _, score) in enumerate(sorted(parsed)):
+    total += i * int(score) + int(score)
+
+print(total)
--- day07/part2.py
+++ day07/part2.py
@@ -1,3 +1,38 @@
-with open("day07/test2.in", "r") as f:
+with open("day07/input.in", "r") as f:
     data = f.readlines()
                 
+order = "J23456789TQKA"
+patterns = [(1, 1, 1, 1, 1), (1, 1, 1, 2), (1, 2, 2), (1, 1, 3), (2, 3), (1, 4), (5,)]
+
+parsed = []
+total = 0
+score_hands = []
+
+for line in data: 
+    hand, score = line.strip().split()
+
+    vals = []
+    evaluated = []
+    for x in hand:
+        vals.append(order.index(x))
+
+    for replacement in order:
+        replaced = hand.replace('J', replacement)
+        count = {}
+        for x in replaced:
+            if x not in count.keys():
+                count[x] = 1
+            else:
+                count[x] += 1
+                
+        pattern = tuple(sorted(count.values()))
+        pattern_val = patterns.index(pattern)
+
+        evaluated.append(pattern_val)
+
+    score_hands.append((max(evaluated), vals, score))
+
+for i, (_, _, score) in enumerate(sorted(score_hands)):
+    total += i * int(score) + int(score)
+
+print(total)
