1002037d6b243ed946fdc4029180324f32884bfc
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-08 05:39:15 +0000

    day08 2023

--- day08/part1.py
+++ day08/part1.py
@@ -1,3 +1,23 @@
-with open("day08/test.in", "r") as f:
+with open("day08/input.in", "r") as f:
     data = f.readlines()
                 
+paths = {}
+
+for line in data[2:]:
+    line = line.strip().split(" ")
+    paths[line[0]] = {
+        "L": line[2][1:-1],
+        "R": line[3][:-1]
+    }
+
+current = "AAA"
+i = 0
+path = data[0].strip()
+
+while True:
+    if current == "ZZZ":
+        break
+    current = paths[current][path[i % len(path)]]
+    i += 1
+
+print(i)
--- day08/part2.py
+++ day08/part2.py
@@ -1,3 +1,28 @@
-with open("day08/test2.in", "r") as f:
+with open("day08/input.in", "r") as f:
     data = f.readlines()
                 
+paths = {}
+
+for line in data[2:]:
+    line = line.strip().split(" ")
+    paths[line[0]] = {
+        "L": line[2][1:-1],
+        "R": line[3][:-1]
+    }
+
+currents = [x for x in paths.keys() if x[-1] == "A"]
+
+path = data[0].strip()
+costs = []
+
+for current in currents:
+    i = 0
+    while True:
+        current = paths[current][path[i % len(path)]]
+        i += 1
+        if current[-1] == "Z":
+            costs.append(i)
+            break
+
+import math
+print(math.lcm(*costs))
