335fe5ee3dc82d354231e441ffabb44b42e3c083
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-12-09 18:03:30 +0000

    day09 2023

--- day09/part1.py
+++ day09/part1.py
@@ -1,3 +1,15 @@
-with open("day09/test.in", "r") as f:
+with open("day09/input.in", "r") as f:
     data = f.readlines()
                 
+res = []
+
+for x in [list(map(int, line.split())) for line in data]:
+    extra = x[-1]
+
+    while x[-1] != 0:
+        x = [h2 - h1 for h1, h2 in zip(x, x[1:])]
+        extra += x[-1]
+
+    res.append(extra)
+
+print(sum(res))
--- day09/part2.py
+++ day09/part2.py
@@ -1,3 +1,21 @@
-with open("day09/test2.in", "r") as f:
+with open("day09/input.in", "r") as f:
     data = f.readlines()
                 
+res = []
+
+for x in [list(map(int, line.split())) for line in data]:
+    count = 0
+    extra = x[0]
+    
+    while any([i != 0 for i in x]):
+        count += 1
+        x = [h2 - h1 for h1, h2 in zip(x, x[1:])]
+
+        if count % 2 == 0:
+            extra += x[0]
+        else:
+            extra -= x[0]
+
+    res.append(extra)
+    
+print(sum(res))
